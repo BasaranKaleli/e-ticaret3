58025d42413c9d7fe9e8e319fdc8edede0d4d528
BasaranKaleli <148017677+BasaranKaleli@users.noreply.github.com>
2023-12-01 07:34:17 +0300

    Ödeme sistemine kart silme ve kart adı değiştirme eklendi

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,  get_object_or_404
 from .models import ShoppingCart, ShoppingCartItem
 
 def shopping_cart_item_add(request, cart_item_id):
@@ -17,3 +17,29 @@ def shopping_cart_item_add(request, cart_item_id):
         shopping_cart.total_price_update()
         shopping_cart.save()
     return redirect('/')
+
+def shopping_cart_item_delete(request, cart_item_id):
+    if request.user.is_authenticated:
+        user = request.user
+        shopping_cart = ShoppingCart.objects.filter(user=user, status="waiting").last()
+        if shopping_cart:
+            item = get_object_or_404(ShoppingCartItem, pk=cart_item_id)
+            shopping_cart.items.remove(item)
+            shopping_cart.total_price_update()
+            shopping_cart.save()
+    return redirect('cart_view')
+
+def shopping_cart_item_edit(request, cart_item_id):
+    if request.user.is_authenticated:
+        user = request.user
+        shopping_cart = ShoppingCart.objects.filter(user=user, status="waiting").last()
+        if shopping_cart:
+            item = get_object_or_404(ShoppingCartItem, pk=cart_item_id)
+            
+            if request.method == 'POST':
+                quantity = request.POST.get('quantity')
+                item.quantity = quantity
+                item.save()
+            shopping_cart.total_price_update()
+            shopping_cart.save()
+    return redirect('cart_view')
