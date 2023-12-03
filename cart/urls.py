b72864b25a39514332a7bf47f632cbc83ae62949
BasaranKaleli <148017677+BasaranKaleli@users.noreply.github.com>
2023-12-03 17:27:12 +0300

    Alışveriş sepeti işlevselliğini düzenlemek için URL şablonları güncellendi

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
-    shopping_cart_item_add
+    shopping_cart_item_add, shopping_cart_item_delete, shopping_cart_item_edit
 )
 
 
 urlpatterns = [
-    path('add/<int:cart_item_id>/', shopping_cart_item_add, name='shopping_cart_item_add'),  
+    path('add/<int:cart_item_id>/', shopping_cart_item_add, name='shopping_cart_item_add'),
+    path('remove/<int:cart_item_id>/', shopping_cart_item_delete, name='shopping_cart_item_remove'),
+    path('edit/<int:cart_item_id>/', shopping_cart_item_edit, name='shopping_cart_item_edit'),  
 ] 
