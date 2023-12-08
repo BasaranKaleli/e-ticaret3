744184990a70b17846f5de45e3ba2672d738487c
BasaranKaleli <148017677+BasaranKaleli@users.noreply.github.com>
2023-12-08 08:20:35 +0300

    Product modeli için admin panelinde kategori bağlantısı ve stok durumu gösterimi eklendi

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
 from .models import Category, Product
-
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
@@ -18,8 +19,6 @@ class CategoryAdmin(admin.ModelAdmin):
         'status', 
     )
 
-
-
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = (
@@ -32,6 +31,8 @@ class ProductAdmin(admin.ModelAdmin):
         'is_home',
         'status', 
         'updated_at',
+        'category_link',  # category_link'i listeye ekledik
+        'stock_status',  # stock_status'u listeye ekledik
     )
     list_filter = ('status', )
     list_editable = (
@@ -39,6 +40,31 @@ class ProductAdmin(admin.ModelAdmin):
         'is_home',
         'status', 
     )
+
+    def category_link(self, obj):
+        return format_html('<a href="{}">{}</a>', reverse('admin:yourapp_category_change', args=[obj.category.pk]), obj.category.title)
+
+    category_link.short_description = 'Kategori'  # Kolon başlığını değiştirir
+
+    actions = ['make_published']
+
+    def make_published(self, request, queryset):
+        rows_updated = queryset.update(status='published')
+        if rows_updated == 1:
+            message_bit = "1 ürün"
+        else:
+            message_bit = f"{rows_updated} ürün"
+        self.message_user(request, f"{message_bit} başarıyla yayınlandı.")
+
+    make_published.short_description = "Seçili ürünleri yayınla"
+
+    def stock_status(self, obj):
+        if obj.stock > 0:
+            return 'Stokta Var'
+        else:
+            return 'Stokta Yok'
+
+    stock_status.short_description = 'Stok Durumu'
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
