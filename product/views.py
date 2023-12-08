124c08d0ae9578e8c7f9b31f8c934a8d7049c1d4
BasaranKaleli <148017677+BasaranKaleli@users.noreply.github.com>
2023-12-08 08:25:45 +0300

    Product kategorisi için detaylı görünüm fonksiyonu eklendi

--- product/views.py
+++ product/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from page.views import STATUS
@@ -6,14 +7,37 @@ def category_show(request, category_slug):
     context = dict()
     context['category'] = get_object_or_404(Category, slug=category_slug)
     
-    # # Nav:
-    # context['categories'] = Category.objects.filter(
-    #     status=STATUS
-    # ).order_by('title')
     context['items'] = Product.objects.filter(
         category=context['category'],
         status=STATUS,
         stock__gte=1,
     )
+
+    
+    page = request.GET.get('page', 1)
+    paginator = Paginator(context['items'], 10) 
+    try:
+        items = paginator.page(page)
+    except PageNotAnInteger:
+        items = paginator.page(1)
+    except EmptyPage:
+        items = paginator.page(paginator.num_pages)
+
+    context['items'] = items
+
+    
+    sort_by = request.GET.get('sort', 'default') 
+    if sort_by == 'price_low':
+        context['items'] = context['items'].order_by('price')
+    elif sort_by == 'price_high':
+        context['items'] = context['items'].order_by('-price')
+    elif sort_by == 'name':
+        context['items'] = context['items'].order_by('title')
+
+    
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    if min_price and max_price:
+        context['items'] = context['items'].filter(price__range=(min_price, max_price))
 
     return render(request, 'product/category_show.html', context)
