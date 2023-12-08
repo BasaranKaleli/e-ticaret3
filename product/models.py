90e0a3001e73f74d4d5ce074c17f9e9891ee94e7
BasaranKaleli <148017677+BasaranKaleli@users.noreply.github.com>
2023-12-08 09:01:33 +0300

    Product modeline renk, beden, resimler, indirimler ve etiketler eklendi

--- product/models.py
+++ product/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from page.models import DEFAULT_STATUS, STATUS
+from django.contrib.auth.models import User
 
 GENDER_CHOICE = [
     ('men', 'Erkek'),
@@ -7,57 +8,52 @@ GENDER_CHOICE = [
     ('unisex', 'UniSex'),
 ]
 
-
 class Category(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(
-        max_length=200, 
-        default="",
-    )
-    status = models.CharField(
-        default=DEFAULT_STATUS, 
-        choices=STATUS,
-        max_length=10,
-    )
-    gender = models.CharField(
-        max_length=6,
-        default="unisex",
-        choices=GENDER_CHOICE,
-    )
-    # order_field = models.SmallIntegerField(default=0)
+    slug = models.SlugField(max_length=200, default="")
+    status = models.CharField(default=DEFAULT_STATUS, choices=STATUS, max_length=10)
+    gender = models.CharField(max_length=6, default="unisex", choices=GENDER_CHOICE)
     createt_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{1000 + self.pk } - {self.gender} - {self.title}"
 
+class Image(models.Model):
+    image = models.ImageField(upload_to='product_images/')
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
 
 class Product(models.Model):
-    category = models.ForeignKey(
-        Category, 
-        on_delete=models.CASCADE
-    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    slug = models.SlugField(
-        max_length=200, 
-        default="",
-    )
+    slug = models.SlugField(max_length=200, default="")
     content = models.TextField() 
-    cover_image = models.ImageField(
-        upload_to='page',
-        null=True,
-        blank=True,
-    )
+    cover_image = models.ImageField(upload_to='page', null=True, blank=True)
     price = models.FloatField()
     stock = models.PositiveSmallIntegerField(default=0)
     is_home = models.BooleanField(default=False)
-    status = models.CharField(
-        default=DEFAULT_STATUS, 
-        choices=STATUS,
-        max_length=10,
-    )
+    status = models.CharField(default=DEFAULT_STATUS, choices=STATUS, max_length=10)
     createt_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    color = models.CharField(max_length=50, null=True, blank=True)
+    size = models.CharField(max_length=20, null=True, blank=True)
+    images = models.ManyToManyField(Image, blank=True)
+    discounts = models.ManyToManyField('Discount', blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return self.title
+
+class Discount(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    discount_percentage = models.FloatField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()  # 1-5 arasında bir değer
+    comment = models.TextField()
