bf397386b6c1fe7c1ed8f52a1fdb352ac9af537f
BasaranKaleli <148017677+BasaranKaleli@users.noreply.github.com>
2023-12-08 08:44:25 +0300

    Oturum süresini kontrol eden ve her istek üzerinde oturum bilgilerini güncelleyen middleware eklendi

--- kaft_clone/middleware.py
+++ kaft_clone/middleware.py
@@ -1,10 +1,28 @@
+import logging
+from datetime import datetime, timedelta
+
+logger = logging.getLogger(__name__)
+
+SESSION_EXPIRATION_TIME = 30  # dakika cinsinden
+
 def session_check_middleware(get_response):
     def middleware(request):
-        if not request.session.session_key:
+        if not request.session.session_key or 'last_activity' not in request.session:
             request.session.save()
-        
-        print(f"SESSION KEY:{request.session.session_key}")
+            request.session['last_activity'] = datetime.now().timestamp()
+        else:
+            last_activity_time = datetime.fromtimestamp(request.session['last_activity'])
+            if datetime.now() - last_activity_time > timedelta(minutes=SESSION_EXPIRATION_TIME):
+                request.session.flush()
+            else:
+                request.session['last_activity'] = datetime.now().timestamp()
+
+        logger.info(f"SESSION KEY: {request.session.session_key}")
+
         response = get_response(request)
+
+        if hasattr(request, 'context_data'):
+            request.context_data['session_key'] = request.session.session_key
 
         return response
 
