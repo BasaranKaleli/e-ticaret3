360e0071ec5c1efa6adac9040efbeb84d73ae3b7
BasaranKaleli <148017677+BasaranKaleli@users.noreply.github.com>
2023-12-22 07:34:38 +0300

    WebSocket üzerinden gerçek zamanlı sohbet uygulamasına kullanıcı bağlantıları, odalara katılım, metin mesajları, sistem bildirimleri ve özel mesaj özelliklerini ekledi

--- asgi/asgi.py
+++ asgi/asgi.py
@@ -3,7 +3,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-from your_app.consumers import YourConsumer
+from your_app.consumers import ChatRoomConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', '<your_project_name>.settings')
 
@@ -13,7 +13,7 @@ application = ProtocolTypeRouter(
         'websocket': AuthMiddlewareStack(
             URLRouter(
                 [
-                    path('ws/some_path/', YourConsumer.as_asgi()),
+                    path('ws/some_path/<room_name>/', ChatRoomConsumer.as_asgi()),
                 ]
             )
         ),
--- asgi/consumers.py
+++ asgi/consumers.py
@@ -1,15 +1,66 @@
-import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+import json
 
-class YourConsumer(AsyncWebsocketConsumer):
+class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f"chat_{room_name}"
+
+        # Kullanıcıyı belirli bir odaya ekleyerek takip edebilirsiniz
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        # Kullanıcının bağlandığını diğer kullanıcılara bildirebilirsiniz
+        await self.send_system_message(f"Kullanıcı '{self.scope['user']}' bağlandı.")
+
         await self.accept()
 
     async def disconnect(self, close_code):
-        pass
+        # Kullanıcıyı odadan çıkararak takibi sonlandırabilirsiniz
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        # Kullanıcının ayrıldığını diğer kullanıcılara bildirebilirsiniz
+        await self.send_system_message(f"Kullanıcı '{self.scope['user']}' ayrıldı.")
 
     async def receive(self, text_data):
         data = json.loads(text_data)
         message = data['message']
 
+        # Alınan mesajı belirli bir odaya yayınlayabilirsiniz
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat.message',
+                'message': message,
+                'user': self.scope['user'],
+            }
+        )
+
+    async def chat_message(self, event):
+        # Odadaki kullanıcılara mesajı yayınlayabilirsiniz
+        message = event['message']
+        user = event['user']
+
+        # Mesajı alan kullanıcıya özel bir biçimde gönderebilirsiniz
+        personalized_message = f"{user}: {message}"
+        await self.send(text_data=json.dumps({'message': personalized_message}))
+
+    async def send_system_message(self, message):
+        # Sistem mesajlarını tüm kullanıcılara gönderebilirsiniz
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'system.message',
+                'message': message,
+            }
+        )
+
+    async def system_message(self, event):
+        # Sistem mesajlarını kullanıcılara gönderebilirsiniz
+        message = event['message']
         await self.send(text_data=json.dumps({'message': message}))
